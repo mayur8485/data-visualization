c368022ef59c1fc43904a802638dd0962c9719b7
Mayuresh <mayuresh.takawane@dista.ai>
2024-06-12 00:48:51 +0530

    adding basic table view

---
+++ src/app/table-view/table-view.component.ts
@@ -0,0 +1,32 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-table-view',
+  templateUrl: './table-view.component.html',
+  styleUrls: ['./table-view.component.css']
+})
+export class TableViewComponent implements OnInit {
+  @Input() data: any;
+
+  dataChunks: any;
+
+  page: number = 0;
+  noOfPages: any = [];
+  recordsPerPage: number = 3;
+  startIndex: number = 0;
+
+  ngOnInit(): void {
+    if (this.data) {
+      this.addPagination(1);
+    }
+  }
+
+  addPagination(page: number) {
+    this.noOfPages = Array.from({ length: Math.ceil(this.data.data.length / this.recordsPerPage) }, (_, index) => index + 1);
+    let startIndex = ((page - 1) * this.recordsPerPage);
+    let endIndex = startIndex + this.recordsPerPage;
+    this.startIndex = startIndex;
+    this.dataChunks = this.data.data.slice(startIndex, endIndex)
+  }
+
+}
