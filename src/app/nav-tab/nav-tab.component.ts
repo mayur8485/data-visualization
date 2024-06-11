62a2cafee02891b3163e2a6d55daccf09899edfd
Mayuresh <mayuresh.takawane@dista.ai>
2024-06-12 00:49:02 +0530

    nav tab

---
+++ src/app/nav-tab/nav-tab.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../services/data.service';
+
+@Component({
+  selector: 'app-nav-tab',
+  templateUrl: './nav-tab.component.html',
+  styleUrls: ['./nav-tab.component.css']
+})
+export class NavTabComponent implements OnInit {
+  active = 0;
+  data: any = [];
+
+  constructor(private dataService: DataService) { }
+  ngOnInit(): void {
+    this.dataService.dataObservable.subscribe((data: any) => {
+      console.log("Data Recevied ",data);
+      this.data = data;
+    })
+    this.data = this.dataService.getData();
+  }
+
+  close($event:any) {
+    $event.stopPropagation();
+    console.log("Close")
+  }
+
+}
