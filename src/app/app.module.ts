2bca40ad5dbff311f44cd3c59b8ded48ecb253f5
Mayuresh <mayuresh.takawane@dista.ai>
2024-06-12 00:49:27 +0530

    services and rest files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TableViewComponent } from './table-view/table-view.component';
+import { HeaderComponent } from './header/header.component';
+import { NavTabComponent } from './nav-tab/nav-tab.component';
+import { NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TableViewComponent,
+    HeaderComponent,
+    NavTabComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbNavModule
   ],
   providers: [],
   bootstrap: [AppComponent]
