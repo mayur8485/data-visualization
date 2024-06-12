9b3244b17af79c2d0efcc39ee77f4c9f96b23b58
Mayuresh <mayuresh.takawane@dista.ai>
2024-06-13 02:05:18 +0530

    adding pie chart

--- src/app/charts/chart/chart.component.ts
+++ src/app/charts/chart/chart.component.ts
@@ -1,31 +1,39 @@
-import { Component, Input, OnInit, Renderer2, Type, ViewChild, ViewContainerRef } from '@angular/core';
+import { Component, Input, OnInit, Type, ViewChild, ViewContainerRef } from '@angular/core';
 import { BarChartComponent } from '../bar-chart/bar-chart.component';
+import { PieChartComponent } from '../pie-chart/pie-chart.component';
+import { DataService } from 'src/app/services/data.service';
 
 @Component({
   selector: 'app-chart',
   templateUrl: './chart.component.html',
-  styleUrls: ['./chart.component.css']
+  styleUrls: ['./chart.component.css'],
+  host: { 'class': 'mb-5 position-relative' }
 })
 export class ChartComponent implements OnInit {
   @ViewChild("chartPlace", { static: true, read: ViewContainerRef }) chartPlace: any;
   @Input() data: any
-  constructor(private viewContainerRef: ViewContainerRef, private renderer2: Renderer2) { }
+  constructor(private viewContainerRef: ViewContainerRef, private dataService: DataService) { }
 
   ngOnInit(): void {
+    this.dataService.dataObservable.subscribe((data: any) => {
+      this.addCharts(this.data);
+    })
     this.addCharts(this.data);
   }
 
   addCharts(data: any) {
     // console.log("Chart Place ", this.data);
     if (data['charts']) {
+      this.chartPlace.clear();
       for (let i = 0; i < data['charts'].length; i++) {
         const component: Type<any> | null = this.getChart(data['charts'][i]['type']);
         if (component) {
           const view = this.viewContainerRef.createComponent(component);
-          console.log(view);
+
+          // set data
           view.instance["data"] = data.data;
           view.instance["property"] = data['charts'][i]
-          view.instance["index"] = i
+          view.instance["index"] = data.id +"_"+ i;
           setTimeout(() => { view.instance.drawChart() }, 0);
           this.chartPlace.insert(view.hostView)
         }
@@ -37,6 +45,7 @@ export class ChartComponent implements OnInit {
   getChart(componentName: string) {
     switch (componentName) {
       case "bar": return BarChartComponent
+      case "pie": return PieChartComponent
       default: return null;
     }
 
