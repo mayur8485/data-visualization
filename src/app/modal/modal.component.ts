fa6b649a2dec6835264b86374f36acf51074d579
Mayuresh <mayuresh.takawane@dista.ai>
2024-06-13 02:05:39 +0530

    modal form for charts

---
+++ src/app/modal/modal.component.ts
@@ -0,0 +1,56 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { DataService } from '../services/data.service';
+
+@Component({
+  selector: 'app-modal',
+  templateUrl: './modal.component.html',
+  styleUrls: ['./modal.component.css']
+})
+export class ModalComponent implements OnInit {
+  @Input() data: any;
+  modalInstance: any;
+  form: any;
+  type: any = [{ key: "bar", value: "Bar" }, { key: "pie", value: "Pie" }];
+
+  constructor(private modalService: NgbModal, private dataService: DataService) { }
+
+  ngOnInit(): void {
+    this.form = new FormGroup({
+      "type": new FormControl(),
+      "height": new FormControl(),
+      "width": new FormControl(),
+      "xAxis": new FormControl(),
+      "yAxis": new FormControl(),
+      "yScale": new FormControl(),
+      "radius": new FormControl(),
+      "fontsize": new FormControl()
+    });
+  }
+
+  createChart() {
+    const property = this.form.value;
+    let newObj: any = {}
+    Object.keys(property).forEach((key: any) => {
+      if (property[key] != null) {
+        newObj[key] = property[key];
+      }
+    })
+
+    this.dataService.addChart(this.data.id, newObj);
+    this.closeModal();
+  }
+
+  openModal(content: any) {
+    this.modalInstance = this.modalService.open(content, { backdrop: true, centered: true });
+  }
+
+  updateModalConfigs() {
+    this.modalInstance.update({ backdrop: false })
+  }
+
+  closeModal() {
+    this.modalInstance.close();
+  }
+}
